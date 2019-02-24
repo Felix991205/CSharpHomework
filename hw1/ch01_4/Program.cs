1167f8a7707e02ea572b5a1f020c0bef1b4e91df
张鉴鼎 <zhangjianding@zhangjiandingdeMacBook-Pro.local>
2019-02-24 09:58:19 +0800

    add hw1 folder and chap01_4

---
+++ hw1/ch01_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ch01_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a=0, b=0;
+            Console.WriteLine("Please input two numbers");
+            a=Convert.ToDouble(Console.ReadLine());
+            b=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The product is "+a*b);
+
+        }
+    }
+}
