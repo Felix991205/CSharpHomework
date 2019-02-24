c21f8f33e1e19d87a701a17906678bd5091c4734
张鉴鼎 <zhangjianding@zhangjiandingdeMacBook-Pro.local>
2019-02-24 10:37:58 +0800

    add ch01_5

---
+++ hw1/ch01_5/ComputeProduct/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputeProduct
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = 0, b = 0;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            label4.Text = "结果为：" + a * b;
+        }
+    }
+}
